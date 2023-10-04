9d14e308631598cb3ada9b0c21282b922d46bfb6
Joe Sandmeyer <github.com@unconscious.com>
2023-10-04 09:49:30 -0400

    v-001

---
+++ hellow/hellow/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  hellow
+//
+//  Created by joe on 10/4/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ hellow/hellow/hellowApp.swift
@@ -0,0 +1,17 @@
+//
+//  hellowApp.swift
+//  hellow
+//
+//  Created by joe on 10/4/23.
+//
+
+import SwiftUI
+
+@main
+struct hellowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
